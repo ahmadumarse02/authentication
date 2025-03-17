7d00aeeb2cc6e8ea5907607a6ca960ee6d1e7ecc
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-03-18 01:41:39 +0500

    login

---
+++ src/actions/login.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import * as z from "zod";
+import { LoginSchema } from "@/schema";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/router";
+import { AuthError } from "next-auth";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validateFields = LoginSchema.safeParse(values);
+
+  if (!validateFields.success) {
+    return { errors: "Invaild Fileds" };
+  }
+
+  const { email, password } = validateFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+
+    return { success: "Login successful!" };
+  } catch (error) {
+    if (error instanceof AuthError)
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    throw error;
+  }
+};
---
+++ src/components/auth/loginForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import React, { useTransition, useState } from "react";
+import CardWrapper from "./cardWrapper";
+import * as z from "zod";
+import { LoginSchema } from "@/schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { login } from "@/actions/login";
+import { FormError } from "@/components/FormError";
+import { FormSuccess } from "@/components/FormSuccess";
+import { useSearchParams } from "next/navigation";
+
+const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with OAuth provider"
+      : "";
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      login(values).then((data) => {
+        setError(data.error);
+      });
+    });
+  };
+
+  return (
+    <>
+      <CardWrapper
+        headerLabel="Welcome back"
+        backButtonLabel="Don't have an account"
+        backButtonHref="/register"
+        showSocial
+      >
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <div className="space-y-4">
+              {/* email field */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter the email"
+                        {...field}
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* password field */}
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="******"
+                        {...field}
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormError message={error || urlError} />
+            <FormSuccess message={success} />
+            <Button type="submit" className="w-full" disabled={isPending}>
+              Login
+            </Button>
+          </form>
+        </Form>
+      </CardWrapper>
+    </>
+  );
+};
+
+export default LoginForm;
