de4baefac325d7ead615be4cf52f208524a30e6a
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-03-22 13:34:49 +0500

    data

--- src/assets/data/data.ts
+++ src/assets/data/data.ts
@@ -15,15 +15,15 @@ import {
 export const menuItems = [
   { name: "Dashboard", href: "/dashboard", icon: Home },
   { name: "Staff", href: "/staff", icon: Users },
-  { name: "Payment Voucher", href: "/payment-voucher", icon: Wallet },
+  { name: "Payment Voucher", href: "/paymentVoucher", icon: Wallet },
   { name: "Payroll", href: "/payroll", icon: FileText },
   { name: "Memo", href: "/memo", icon: ClipboardList },
   { name: "Circulars", href: "/circulars", icon: FileText },
   { name: "Maintenance", href: "/maintenance", icon: Wrench },
   { name: "Logistics", href: "/logistics", icon: Truck },
-  { name: "Office Budget", href: "/office-budget", icon: Wallet },
+  { name: "Office Budget", href: "/officeBudget", icon: Wallet },
   { name: "Stocks and Inventory", href: "/stocks", icon: Package },
   { name: "Notifications", href: "/notifications", icon: Bell },
-  { name: "Capacity Building", href: "/capacity-building", icon: Building2 },
+  { name: "Capacity Building", href: "/capacityBuilding", icon: Building2 },
   { name: "Procurements", href: "/procurements", icon: Settings },
 ];
